58702ef86abb2c422532aa274619983e9afc8d30
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-15 22:10:21 +0300

    Class User created

--- TornamentManager/AutorizationLogic/User.cs
+++ TornamentManager/AutorizationLogic/User.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace TornamentManager.AutorizationLogic
 {
-    public class User
+    public class User : IUser
     {
-
+        string IUser.Login { get ; set ; }
+        string IUser.Password { get ; set ; }
+        EUserPrivileges IUser.UserPrivilages { get ; set ; }
     }
 }
