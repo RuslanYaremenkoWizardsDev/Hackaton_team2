7bdffc403e68ebe125dbe7a8c628538a25d63a24
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-15 17:49:46 +0300

    Created Enum TournamentTypes

---
+++ TornamentManager/InternalLogic/Tournament/ETournamentTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornamentManager
+{
+    public enum ETournamentTypes
+    {
+        Cup=1,
+        Championship=2
+    }
+}
