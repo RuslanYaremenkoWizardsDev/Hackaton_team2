7c69ab9aab3fbd2ee66f0bf1529182656fb648be
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-15 19:12:41 +0300

    Created Class TournamentBase

---
+++ TornamentManager/InternalLogic/Tournament/TournamentBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornamentManager
+{
+    public class TournamentBase : ITournament
+    {
+        
+        string ITournament.Name { get; set ; }
+        string ITournament.Description { get ; set ; }
+        ETournamentModes ITournament.TornamentMode { get ; set ; }
+        string ITournament.Place { get ; set}
+        DateTime ITournament.StartDateTime { get; set ; }
+        DateTime ITournament.LastRegistrationDateTime { get ; set; }
+        ETournamentLevel ITournament.TournamentLevel { get ; set ; }
+        int ITournament.NumberOfParticipants { get ; set ; }
+        ETournamentScenarios ITournament.Scenario { get ; set ; }
+
+        IList<ITeamClass> ITournament.Players { get; }
+    }
+}
