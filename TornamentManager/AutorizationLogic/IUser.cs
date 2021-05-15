54aa9990110a391a8d1cb0dae90bb3b22e665936
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-15 22:09:34 +0300

    IUser Interface created

---
+++ TornamentManager/AutorizationLogic/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornamentManager.AutorizationLogic
+{
+    public interface IUser
+    {
+        string Login { get; set; }
+        string Password { get; set; }
+
+        EUserPrivileges UserPrivilages { get; set; }
+    }
+}
