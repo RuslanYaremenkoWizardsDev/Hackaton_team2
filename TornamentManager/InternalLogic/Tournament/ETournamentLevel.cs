f6cae949df83c604561759d78be71f9babdb400e
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-15 18:00:49 +0300

    Added Enum EtournamentLevel

---
+++ TornamentManager/InternalLogic/Tournament/ETournamentLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornamentManager.InternalLogic.Tournament
+{
+    public enum ETournamentLevel
+    {
+        Advanced =1,
+        Middle = 2,
+        Beginner=3
+    }
+}
