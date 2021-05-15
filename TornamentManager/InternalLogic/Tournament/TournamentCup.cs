3bf7545b8367e13ca18dbe7ff76597e4381a650d
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-15 19:55:04 +0300

    TournamentCup Class created

---
+++ TornamentManager/InternalLogic/Tournament/TournamentCup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornamentManager
+{
+    public class TournamentCup: TournamentBase
+
+    {
+        public TournamentCup(string name, ETournamentModes tournamentMode, int numberOfParticipants)
+            : base(name, tournamentMode, numberOfParticipants)
+        {
+
+        }
+            
+    }
+}
