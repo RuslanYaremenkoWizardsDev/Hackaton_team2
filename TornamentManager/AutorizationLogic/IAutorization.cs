c242bb461163a08c1bab5096b7f8e1e3cb037fc4
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-16 10:27:32 +0300

    Interface IAutorization extended: ChangeUserPassword

--- TornamentManager/AutorizationLogic/IAutorization.cs
+++ TornamentManager/AutorizationLogic/IAutorization.cs
@@ -13,5 +13,9 @@ namespace TornamentManager.AutorizationLogic
 
         void SaveUsers(StreamWriter streamWriter);
         IActiveUser AutorizeUser(string login, string password);
+
+        IUser CreateUser(string login, string password, EUserPrivileges userPrivilages);
+
+        bool ChangeUserPassword(string login, string oldPassword, string newPassword);
     }
 }
