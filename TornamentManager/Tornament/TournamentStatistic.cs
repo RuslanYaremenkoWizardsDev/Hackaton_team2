27827b0f161ea86e20a9f633bdbc594e96f76df8
Pavel <pavelbig9999@gmail.com>
2021-05-16 12:39:00 +0300

    Finished Tournaments stats page

--- TornamentManager/Tornament/TournamentStatistic.cs
+++ TornamentManager/Tornament/TournamentStatistic.cs
@@ -6,47 +6,27 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-
+using TornamentManager.Stats;
 
 namespace TornamentManager.Tornament
 {
     public class TournamentStatistic : MainWindow
-    {        
-        int CupNumber { get; set; }
-        int CupActive { get; set; }
-        int CupFinished { get; set; }
-        int CupCancelled { get; set; }
-        int ChampionshipNumber { get; set; }
-        int ChampionshipActive { get; set; }
-        int ChampionshipFinished { get; set; }
-        int ChampionshipCancelled { get; set; }
-        int totalNumber;
-        int totalActive;
-        int totalFinished;
-        int totalCancelled;
-        
+    {
+        TournamentsStatistics tournamentsStatistics = new TournamentsStatistics();
         public TournamentStatistic()
         {
-            while (true)
-            {
-                
-            }
-            totalNumber = CupNumber + ChampionshipNumber;
-            totalActive = CupActive + ChampionshipActive;
-            totalFinished = CupFinished + ChampionshipFinished;
-            totalCancelled = CupCancelled + ChampionshipCancelled;
-            CupNumber_textBox.Text = Convert.ToString(CupNumber);
-            CupActive_textBox.Text = Convert.ToString(CupActive);
-            CupFinished_textBox.Text = Convert.ToString(CupFinished);
-            CupCancelled_textBox.Text = Convert.ToString(CupCancelled);
-            ChampionshipNumber_textBox.Text = Convert.ToString(ChampionshipNumber);
-            ChampionshipActive_textBox.Text = Convert.ToString(ChampionshipActive);
-            ChampionshipFinished_textBox.Text = Convert.ToString(ChampionshipFinished);
-            ChampionshipCancelled_textBox.Text = Convert.ToString(ChampionshipCancelled);
-            TotalNumber_textBox.Text = Convert.ToString(totalNumber);
-            TotalActive_textBox.Text = Convert.ToString(totalActive);
-            TotalFinished_textBox.Text = Convert.ToString(totalFinished);
-            TotalCancelled_textBox.Text = Convert.ToString(totalCancelled);
+            CupNumber_textBox.Text = Convert.ToString(tournamentsStatistics.CupStats.Number);
+            CupActive_textBox.Text = Convert.ToString(tournamentsStatistics.CupStats.Active);
+            CupFinished_textBox.Text = Convert.ToString(tournamentsStatistics.CupStats.Finished);
+            CupCancelled_textBox.Text = Convert.ToString(tournamentsStatistics.CupStats.Cancelled);
+            ChampionshipNumber_textBox.Text = Convert.ToString(tournamentsStatistics.ChampionshipStats.Number);
+            ChampionshipActive_textBox.Text = Convert.ToString(tournamentsStatistics.ChampionshipStats.Active);
+            ChampionshipFinished_textBox.Text = Convert.ToString(tournamentsStatistics.ChampionshipStats.Finished);
+            ChampionshipCancelled_textBox.Text = Convert.ToString(tournamentsStatistics.ChampionshipStats.Cancelled);
+            TotalNumber_textBox.Text = Convert.ToString(tournamentsStatistics.TotalStats.Number);
+            TotalActive_textBox.Text = Convert.ToString(tournamentsStatistics.TotalStats.Active);
+            TotalFinished_textBox.Text = Convert.ToString(tournamentsStatistics.TotalStats.Finished);
+            TotalCancelled_textBox.Text = Convert.ToString(tournamentsStatistics.TotalStats.Cancelled);
         }
     }
 }
