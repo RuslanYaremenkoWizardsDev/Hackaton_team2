a0ecf986a907975da57fb1b4841a81f1d891e94c
Serhii <avakyanms@gmail.com>
2021-05-16 22:25:18 +0300

    Added tests CreateUser_WhenLoginPasswordUserPrivilages_ShouldFalse

--- TestProject1Test/AutorizationTests.cs
+++ TestProject1Test/AutorizationTests.cs
@@ -16,11 +16,12 @@ namespace TournamentManager.Tests
         [TestCase("User")]
         [TestCase("sergey")]
         [TestCase("A")]
-        public void ValidateLogin_test_WhenTrue(string login)
+        public void ValidateLogin_WhenLogin_ShouldTrue(string login)
         {
             IAutorization autorization = new Autorization();
             bool actual = autorization.validateLogin(login);
             bool expected = true;
+
             Assert.AreEqual(actual, expected);
         }
 
@@ -29,11 +30,12 @@ namespace TournamentManager.Tests
         [TestCase("1_g")]
         [TestCase("d d")]
         [TestCase("gfff ")]
-        public void ValidateLogin_test_WhenFalse(string login)
+        public void ValidateLogin_WhenLogin_ShouldFalse(string login)
         {
             IAutorization autorization = new Autorization();
             bool actual = autorization.validateLogin(login);
             bool expected = false;
+
             Assert.AreEqual(actual, expected);
         }
 
@@ -41,11 +43,12 @@ namespace TournamentManager.Tests
         [TestCase("User")]
         [TestCase("sergey")]
         [TestCase("Avahhh")]
-        public void ValidatePassword_test_Valid(string login)
+        public void ValidatePassword_WhenPassword_ShouldValid(string password)
         {
             IAutorization autorization = new Autorization();
-            bool actual = autorization.validateLogin(login);
+            bool actual = autorization.validatePassword(password);
             bool expected = true;
+
             Assert.AreEqual(actual, expected);
         }
 
@@ -54,11 +57,12 @@ namespace TournamentManager.Tests
         [TestCase("1")]
         [TestCase("d d")]
         [TestCase("gfff ")]
-        public void ValidatePassword_test_Invalid(string login)
+        public void ValidatePassword_WhenPassword_ShouldInvalid(string password)
         {
             IAutorization autorization = new Autorization();
-            bool actual = autorization.validateLogin(login);
+            bool actual = autorization.validatePassword(password);
             bool expected = false;
+
             Assert.AreEqual(actual, expected);
         }
         [TestCase("Admin", "Admin", EUserPrivileges.Admin)]
@@ -75,16 +79,26 @@ namespace TournamentManager.Tests
             Assert.AreEqual(actual.UserPrivilages, userPrivilages);
         }
 
-        [TestCase("A dmin", "Admin", EUserPrivileges.Admin)]
+        [TestCase("Admin", "Admin", EUserPrivileges.Admin)]
+        public void CreateUser_WhenDuplicateLogin_ShouldFalse(string login, string password, EUserPrivileges userPrivilages)
+        {
+            IAutorization autorization = new Autorization();
+            IUser actual = autorization.CreateUser(login, password, userPrivilages);
+            
+            Assert.IsNotNull(actual);
+            actual = autorization.CreateUser(login, password, userPrivilages);
+            Assert.Null(actual);
+        }
+
+        [TestCase("mi", "", EUserPrivileges.Admin)]
+        [TestCase("1", "11", EUserPrivileges.Admin)]
+        [TestCase("@@", "11", EUserPrivileges.Admin)]
         public void CreateUser_WhenLoginPasswordUserPrivilages_ShouldFalse(string login, string password, EUserPrivileges userPrivilages)
         {
             IAutorization autorization = new Autorization();
             IUser actual = autorization.CreateUser(login, password, userPrivilages);
 
-            Assert.IsNotNull(actual);
-            Assert.AreEqual(actual.Login, login);
-            Assert.AreEqual(actual.Password, password);
-            Assert.AreEqual(actual.UserPrivilages, userPrivilages);
+            Assert.IsNull(actual);
         }
     }
 }
