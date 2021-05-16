e162230f97d824d97fd1d3fe1e33c9b1cffb0f80
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-16 11:18:03 +0300

    Class TournamentStatsInfo created

---
+++ TornamentManager/Stats/TournamentStatsInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TornamentManager.Stats
+{
+    public class TournamentStatsInfo
+    {
+        public int Number;
+        public int Active;
+        public int Finished;
+        public int Cancelled;
+    }
+}
