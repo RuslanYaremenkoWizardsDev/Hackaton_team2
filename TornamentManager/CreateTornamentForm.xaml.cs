bc4d793a05fd3d621dca2f142f35d2c13915f7a0
Maks Kovalenko (Lenovo) <max.don@live.ru>
2021-05-16 11:37:19 +0300

    Add level, numbers of participants and Scenarios

--- TornamentManager/CreateTornamentForm.xaml.cs
+++ TornamentManager/CreateTornamentForm.xaml.cs
@@ -21,18 +21,90 @@ namespace TornamentManager
     public partial class CreateTournamentForm : Window
     {
         private ObservableCollection<ETournamentModes> _modes;
+        private ObservableCollection<ETournamentLevel> _levels;
+
         public CreateTournamentForm()
         {
             _modes = new ObservableCollection<ETournamentModes>((IEnumerable<ETournamentModes>)Enum.GetValues(typeof(ETournamentModes)));
+            _levels = new ObservableCollection<ETournamentLevel>((IEnumerable<ETournamentLevel>)Enum.GetValues(typeof(ETournamentLevel)));
+
             InitializeComponent();
             TornamentModesComboBox.ItemsSource = _modes;
             TornamentModesComboBox.SelectedItem = _modes[0];
+
+            TournamentLevelsComboBox.ItemsSource = _levels;
+            TournamentLevelsComboBox.SelectedItem = _levels[1];
             StartDatePicker.Minimum = DateTime.Now;
+
+            ObservableCollection<int> numberOfParticipants;
+            numberOfParticipants = Get_NumberOfParticipantsList();
+            NumberOfParticipantsComboBox.ItemsSource = numberOfParticipants;
+            NumberOfParticipantsComboBox.SelectedItem = numberOfParticipants[numberOfParticipants.Count - 1];
+
+            ObservableCollection<ETournamentScenarios> scenarios = GetTournamentScenariosComboBoxItems();
+            TournamentScenariosComboBox.ItemsSource = scenarios;
+            TournamentScenariosComboBox.SelectedItem = scenarios[0];
+        }
+        private void TornamentModesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ObservableCollection<int> numberOfParticipants;
+            numberOfParticipants = Get_NumberOfParticipantsList();
+            NumberOfParticipantsComboBox.ItemsSource = numberOfParticipants;
+            NumberOfParticipantsComboBox.SelectedItem = numberOfParticipants[numberOfParticipants.Count - 1];
+
+            ObservableCollection<ETournamentScenarios> scenarios = GetTournamentScenariosComboBoxItems();
+
+            TournamentScenariosComboBox.ItemsSource = scenarios;
+            TournamentScenariosComboBox.SelectedItem = scenarios[0];
         }
 
         private void StartDatePicker_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             LastRegistrationDate.Maximum = StartDatePicker.Value;
+        }
+
+        private ObservableCollection<ETournamentScenarios> GetTournamentScenariosComboBoxItems()
+        {
+            ObservableCollection<ETournamentScenarios> scenarios = new ObservableCollection<ETournamentScenarios>(
+                (IEnumerable<ETournamentScenarios>)Enum.GetValues(typeof(ETournamentScenarios)));
+
+            if (TornamentModesComboBox.SelectedIndex == 0)
+            {
+                scenarios = new ObservableCollection<ETournamentScenarios>((IEnumerable<ETournamentScenarios>)Enum.GetValues(typeof(ETournamentScenarios)));
+            }
+            else
+            {
+                scenarios = new ObservableCollection<ETournamentScenarios>();
+                scenarios.Add(ETournamentScenarios.OneMatchConfrontation);
+            }
+            
+            return scenarios;
+        }
+
+        private ObservableCollection<int> Get_NumberOfParticipantsList()
+        {
+            ObservableCollection<int> numberOfParticipants = new ObservableCollection<int>();
+            switch (TornamentModesComboBox.SelectedIndex)
+            {
+                case 0:
+                    numberOfParticipants.Add(4);
+                    numberOfParticipants.Add(8);
+                    numberOfParticipants.Add(16);
+                    numberOfParticipants.Add(32);
+                    numberOfParticipants.Add(64);
+                    numberOfParticipants.Add(128);
+                    break;
+                case 1:
+
+                    for (int i = 1; i <= 10; ++i)
+                    {
+                        numberOfParticipants.Add(i);
+                    }
+
+                    break;
+            }
+
+            return numberOfParticipants;
         }
     }
 }
