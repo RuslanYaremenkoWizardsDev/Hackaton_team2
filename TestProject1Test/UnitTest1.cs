38c113c12403407bcb2d7ecc81f6254abc00400e
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-16 18:35:02 +0300

    Created NUnit TestProject

---
+++ TestProject1Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using TornamentManager.AutorizationLogic;
+
+
+namespace TournamentManager.Tests
+{
+    public class AutorizationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TestCase("Admin")]
+        public void ValidateLogin_test_WhenTrue(string login)
+        {
+            IAutorization autorization = new Auto
+            
+        }
+    }
+}
