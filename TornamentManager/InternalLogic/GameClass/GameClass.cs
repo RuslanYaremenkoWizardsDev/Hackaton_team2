c746bc6a31fdc957a7a75c4463c5d9f581bb89f1
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-17 04:23:46 +0300

    Added GameClass

--- TornamentManager/InternalLogic/GameClass/GameClass.cs
+++ TornamentManager/InternalLogic/GameClass/GameClass.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace TornamentManager
 {
-    class GameClass: IGameClass
+    class GameClass : IGameClass
     {
+        private static int _lastID = 0;
+
+        public GameClass(ITournament tournament)
+        {
+            Tournament = tournament;
+            _lastID++;
+            ID = _lastID;
+        }
+        public ITeamClass Team1 { get ; set ; }
+        public ITeamClass Team2 { get ; set ; }
+
+        public ITournament Tournament { get; private set; }
+
+        public EGameState GameState { get ; set ; }
+        public EGameResult GameResult { get ; set ; }
+
+        public int ID { get; private set; }
     }
 }
