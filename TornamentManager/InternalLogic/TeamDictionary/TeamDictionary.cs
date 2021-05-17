4d9596968e4378b6f769fe57538472d1624f9c79
Serhii <avakyanms@gmail.com>
2021-05-17 03:52:06 +0300

    Implemented class TeamList

--- TornamentManager/InternalLogic/TeamDictionary/TeamDictionary.cs
+++ TornamentManager/InternalLogic/TeamDictionary/TeamDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,10 +8,61 @@ using System.Threading.Tasks;
 
 namespace TornamentManager
 {
-   public class TeamDictionary : ITeamDictionary
+    public class TeamDictionary : ITeamDictionary
     {
         private IList<ITeamClass> _teamList;
-        IList<ITeamClass> ITeamDictionary.TeamList => _teamList;
+
+        public event ITeamDictionary.ITeamEvent TeamAdded;
+        public event ITeamDictionary.VoidEvent TTeamRemoved;
+        public event ITeamDictionary.VoidEvent TeamListChanged;
+
+        public void AddTeam(ITeamClass team)
+        {
+            _teamList.Add(team);
+            TeamListChanged?.Invoke();
+            TeamAdded?.Invoke(team);
+        }
+
+        public IEnumerator<ITeamClass> GetEnumerator()
+        {
+            return _teamList.GetEnumerator();
+        }
+
+        public ITeamClass GetTeamByID(int ID)
+        {
+            foreach (var e in _teamList)
+            {
+                if (e.ID == ID)
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public void RemoveByID(int ID)
+        {
+            foreach (var e in _teamList)
+            {
+                if (e.ID == ID)
+                {
+                    _teamList.Remove(e);
+                    TTeamRemoved?.Invoke();
+                    TeamListChanged?.Invoke();
+                    break;
+                }
+            }
+        }
+
+        public void TriggerListChangeEvent()
+        {
+            TeamListChanged?.Invoke();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _teamList.GetEnumerator();
+        }
 
         void ITeamDictionary.LoadFromFile(StreamReader streamReader)
         {
@@ -20,6 +72,12 @@ namespace TornamentManager
         void ITeamDictionary.SaveToFile(StreamWriter streamWriter)
         {
             throw new NotImplementedException();
+        }
+
+        public TeamDictionary()
+        {
+            _teamList = new List<ITeamClass>();
+
         }
     }
 }
