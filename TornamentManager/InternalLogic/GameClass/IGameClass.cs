d02f356553ff310be16240b4395d0dd9ba9cb536
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-17 04:10:50 +0300

    ENum EGameState created

--- TornamentManager/InternalLogic/GameClass/IGameClass.cs
+++ TornamentManager/InternalLogic/GameClass/IGameClass.cs
@@ -8,6 +8,12 @@ namespace TornamentManager
 {
     public interface IGameClass
     {
+        public ITeamClass Team1 { get; set; }
+        public ITeamClass Team2 { get; set; }
+
+        public ITournament Tournament { get; }
+
+
 
     }
 }
